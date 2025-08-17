c247c45d3de24217ea23b8e91c6efc7b88e02cc7
Rakshith <mlrakshi1@gmail.com>
2025-08-17 22:43:22 +0530

    update login-2

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
 
-  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-emerald-100 to-emerald-800">
       <div className="bg-white shadow-2xl rounded-2xl p-10 w-96">
         <h2 className="text-3xl font-bold text-center text-emerald-600 mb-6">
           Welcome Back
         </h2>
-        <form className="flex flex-col gap-5">
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col gap-5"
+        >
           <input
+            onChange={() => {
+              console.log("changing");
+            }}
             type="email"
             placeholder="Email"
             className="border-2 border-emerald-300 rounded-xl px-4 py-3 outline-none focus:border-emerald-500"
